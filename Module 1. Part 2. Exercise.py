f1c98e88328a34c706ce190ef23dab96ab920bd2
EmaBrr <emabartasyte@gmail.com>
2023-10-02 18:36:04 +0300

    hackerrank

--- Module 1. Part 2. Exercise.py
+++ Module 1. Part 2. Exercise.py
@@ -48,3 +48,44 @@ pig_latin()
 #         print("Input is not a string.")
 
 # pig_latin()
+
+
+# Exercise in HackerRank
+
+from collections import deque
+
+def deque_f():
+    try:
+        action_count = int(input("How many actions you want to do with deque: "))
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+        exit(1)
+    
+    my_deque = deque()
+
+    for i in range(action_count):
+        action = input("Action that you want to do with deque: ")
+        words = action.split()
+        action_on_deque = words[0]
+
+        if len(words) == 2:
+            number = words[1] 
+        
+            if action_on_deque.lower() == 'append':
+                my_deque.append(number)
+            elif action_on_deque.lower() == 'appendleft':
+                my_deque.appendleft(number)
+
+        elif len(words) == 1 and len(my_deque) != 0 :
+            if action_on_deque.lower() == 'pop':
+                my_deque.pop()
+            elif action_on_deque.lower() == 'popleft':   
+                my_deque.popleft()
+        else:
+            'Input is incorrect'
+    elements_as_string = ' '.join(map(str, my_deque))
+    print(elements_as_string)
+
+
+deque_f()   
+
